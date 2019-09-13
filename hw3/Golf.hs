ed0de4214d11e47eb94dad6d12e105198490274a
Chirag Davé <c@chirag.io>
2019-09-13 09:46:52 -0400

    no need for flip

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -3,10 +3,13 @@ module Golf where
 
 -- Hopscotch
 
--- every :: Int -> [a] -> [a]
--- every n l = map fst [x | x <- zip l [1..], (snd x) `mod` n == 0]
-
 skips :: [a] -> [[a]]
-skips l = map (flip every l ) [1..length(l)]
+skips l = map (every l ) [1..length(l)]
   where
-    every n lst = map fst [x | x <- zip lst [1..], (snd x) `mod` n == 0]
+    every lst n = map fst [x | x <- zip lst [1..], (snd x) `mod` n == 0]
+
+-- Local Maxima
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima (_:_) = []
